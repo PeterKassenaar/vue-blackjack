8c8c6995256a5616781c29872379f67a3014bf3f
Peter Kassenaar <info@kassenaar.com>
2019-03-30 10:46:21 +0100

    Updated rules

---
+++ src/data/rules.js
@@ -0,0 +1,8 @@
+// rules.js - various rules, shown as tooltips
+const rules = {
+	canDouble: 'Player can double if he has only two cards, totalling 9, 10 or 11.',
+	canSplit: 'Player can split if he has only two cards, which have the same value.',
+	hit: 'Draw a new card.',
+	stand: 'Stand, let the dealer play'
+};
+export default rules;
